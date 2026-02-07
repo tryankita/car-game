38fe10ce558bf6e182df0c55936bdf1c5bd1fa2d
tryankita <try.ankitasharma@gmail.com>
2026-02-07 17:21:48 +0530

    Update car game files

---
+++ fix_remote.bat
@@ -0,0 +1,4 @@
+@echo off
+git remote set-url origin https://github.com/tryankita/car-game.git
+git remote -v
+git push -u origin main --force
--- setup_git.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git init
-git remote remove origin
-git remote add origin https://github.com/tryankita/car-game.git
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git push -u origin main
